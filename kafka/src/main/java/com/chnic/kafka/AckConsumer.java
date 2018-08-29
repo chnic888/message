86296828e21fc11014412c2650c2b73673d10955
chnic888 <chnic888@gmail.com>
2018-08-29 23:56:06 +0800

    add manual commit consumer

--- kafka/src/main/java/com/chnic/kafka/AckConsumer.java
+++ kafka/src/main/java/com/chnic/kafka/AckConsumer.java
@@ -9,7 +9,7 @@ import java.util.Collections;
 import java.util.Properties;
 
 @Slf4j
-public class Consumer {
+public class AckConsumer {
 
     private static final String TOPIC_NAME = "test.topic.1";
 
@@ -20,21 +20,31 @@ public class Consumer {
         properties.put("group.id", "Tester");
         properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
         properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
-        properties.put("enable.auto.commit", "true");
-        properties.put("auto.commit.interval.ms", "3000");
+        properties.put("enable.auto.commit", "false");
 
         KafkaConsumer<String, String> kafkaConsumer = new KafkaConsumer<>(properties);
         kafkaConsumer.subscribe(Collections.singletonList(TOPIC_NAME));
 
-        //subscribe topic by regex
-//        kafkaConsumer.subscribe(Pattern.compile("test.*"));
-
         try {
             while (true) {
-                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(100));
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(10000));
                 consumerRecords.forEach(record -> {
                     log.info("partition: {}, offset: {}, key {}, value: {}", record.partition(), record.offset(), record.key(), record.value());
                 });
+
+//                kafkaConsumer.commitSync();
+//                log.info("commitSync...");
+
+//                kafkaConsumer.commitAsync();
+//                log.info("commitAsync...");
+
+                kafkaConsumer.commitAsync(((offsets, exception) -> {
+                    if (exception != null) {
+                        log.error("commitAsync with callback, {}", exception.getMessage());
+                    } else {
+                        log.info("commitAsync with callback, {}", offsets);
+                    }
+                }));
             }
         } finally {
             kafkaConsumer.close();
